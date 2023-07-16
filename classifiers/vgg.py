9bde5c3af1d57b364fa51f5ac14ec2400ab0801d
prapti1112 <praptipatil01@gmail.com>
2023-07-16 16:03:05 +0530

    VGG model building and training added

---
+++ classifiers/vgg.py
@@ -0,0 +1,64 @@
+"""
+    This is a code to recreate the model architecture given in the 
+    Paper: VERY DEEP CONVOLUTIONAL NETWORKS FOR LARGE-SCALE IMAGE RECOGNITION [https://arxiv.org/pdf/1409.1556v6.pdf]
+"""
+
+import logging
+import tensorflow as tf
+from tensorflow.python.keras import Model
+from tensorflow.python.keras.layers import Input, Conv2D, MaxPool2D, Dense, Flatten, Layer, Dropout
+from tensorflow.python.keras.losses import SparseCategoricalCrossentropy
+from tensorflow.python.keras.optimizers import gradient_descent_v2
+
+class VGG:
+    def __init__(self) -> None:
+        self.compiler_params = {
+            "optimizer": gradient_descent_v2.SGD(learning_rate=0.01,  momentum=0.0, decay=0.1),
+            "loss": SparseCategoricalCrossentropy(from_logits=True),
+            "metrics": ['accuracy'],
+            "learning_rate": 0.1,
+        }
+        logging.info("Initializing vgg network")
+    
+    class Conv_Block(Layer):
+        def __init__(self, num_filters, block_num, conv_per_block=2):
+            super().__init__()
+            self.num_filters = num_filters
+            self.block_number = block_num
+            self.conv_per_block = conv_per_block
+        
+        def __call__(self, input_tensor):
+            x = Conv2D(self.num_filters, 3, activation="relu", name = f"Conv_{self.block_number}_1")(input_tensor)
+            for i in range(2, self.conv_per_block+1):
+                x = Conv2D( self.num_filters, 3, activation="relu", padding="same", name = f"Conv_{self.block_number}_{i}" )(x)
+            x = MaxPool2D(strides=2, padding="same", name=f"MaxPooling_{self.block_number}")(x)
+            
+            return x
+
+    def build_model(self, input_size, num_classes):
+        """
+            This function build the vgg-19 model using the function api
+            @param[in]: input_size(int)
+            @param[in]: num_classes(int) - output classes
+            ---
+            @param[out]: model(tf.keras.Model)
+        """
+        logging.debug("Starting to build vgg19 model")
+
+        input = Input(shape = input_size)
+        
+        x = self.Conv_Block(64, 1)(input) 
+        x = self.Conv_Block(128, 2)(x) 
+        x = self.Conv_Block(256, 3, 3)(x)
+        x = self.Conv_Block(512, 4, 3)(x)
+        x = self.Conv_Block(512, 5, 3)(x)
+
+        x = Flatten()(x)
+        x = Dense(4096, activation='relu')(x)
+        x = Dropout(0.5)(x)
+        x = Dense(4096, activation='relu')(x)
+        x = Dropout(0.5)(x)
+        x = Dense(num_classes, activation='softmax')(x)
+        model = Model( inputs=input, outputs=x )
+        
+        return model
--- image_classification.py
+++ image_classification.py
@@ -1,11 +1,18 @@
 import logging
 import os
 import tensorflow as tf
+from tensorflow.python.keras.callbacks import ModelCheckpoint
 
 class Image_Classification:
     def __init__(self, model_name="vgg19") -> None:
         self.IMAGE_SIZE = 224
         logging.info(f"Starting image classification with {model_name}")
+
+        self.weights_path = r"C:\Users\prapt\Desktop\Deep Learning\Image_classification_Nature\pretrained_weights"
+        if model_name == "vgg19":
+            from classifiers.vgg import VGG
+            self.model = VGG()
+            self.weights_path = os.path.join(self.weights_path, "vgg19_weights_tf_dim_ordering_tf_kernels_notop.h5")
 
     def load_data(self, data_path):
         """
@@ -17,9 +24,10 @@ class Image_Classification:
         logging.info(f"Loading data from: {data_path}")
         dataset = tf.keras.utils.image_dataset_from_directory(data_path, image_size=(self.IMAGE_SIZE, self.IMAGE_SIZE))
         dataset = dataset.prefetch(tf.data.AUTOTUNE) # type: ignore
+
         return dataset
 
-    def train(self, data_path):
+    def train(self, data_path, model_save_path):
         """
             Function to train an image classifier
             @param[in]: data_path(String/PathLike) - Path to where the data is store.
@@ -41,14 +49,19 @@ class Image_Classification:
             |           :
         """
         # TODO: Validate the data path entered
-        train_dataset = self.load_data(os.path.join(data_path, "train"))
-        validation_dataset = self.load_data(os.path.join(data_path, "val"))
+        with tf.device('/device:gpu:0'):
+            train_dataset = self.load_data(os.path.join(data_path, "train"))
+            validation_dataset = self.load_data(os.path.join(data_path, "val"))
 
-        # Testing datasets
-        for (image, label) in train_dataset.take(3):
-            print( image.shape, label )
+        model = self.model.build_model((self.IMAGE_SIZE, self.IMAGE_SIZE, 3), 10)
+        model.summary()
 
+        model.compile(optimizer=self.model.compiler_params["optimizer"], loss=self.model.compiler_params["loss"], metrics=self.model.compiler_params["metrics"] )
 
+        with tf.device('/device:gpu:0'):
+            model.fit( train_dataset.take(5), validation_data=validation_dataset.take(5), epochs=3, use_multiprocessing=True,
+                      callbacks=[ModelCheckpoint( os.path.join(model_save_path, "vgg19_{epoch:03d}-{val_loss:.4f}.h5" ),  save_weights_only = True) ] )
+        
     def infer(self):
         pass
 
@@ -56,4 +69,4 @@ class Image_Classification:
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     classifier = Image_Classification()
-    classifier.train(r"C:\Users\prapt\Desktop\Deep Learning\Image_classification_Nature\data\inaturalist_12K")
+    classifier.train(r"C:\Users\prapt\Desktop\Deep Learning\Image_classification_Nature\data\inaturalist_12K", r"C:\Users\prapt\Desktop\Deep Learning\Image_classification_Nature\models")
